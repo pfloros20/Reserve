7a929bfbb860956ead2d1c48367026117f93b30b
george521 <george.aggelopoulos@hotmail.com>
2019-05-15 12:16:09 +0300

    Added Admin Java File & Owner Java File.

---
+++ Android/Admin.java
@@ -0,0 +1,14 @@
+package com.example.admin;
+
+public class Admin {
+    int ID;
+    String Username;
+    String Password;
+
+    public Admin(int ID, String Username, String Password){
+        this.ID = ID;
+        this.Username = Username;
+        this.Password = Password;
+    }
+
+}
